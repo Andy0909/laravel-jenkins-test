83e53f24fe7fe5e28e780243d23d65d14a307cfd
Andy Wei <48671893+Andy0909@users.noreply.github.com>
2023-09-09 23:37:07 +0800

    還原檔案

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,7 +31,7 @@
                 </div>
             @endif
 
-            test git
+            
 
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex justify-center">
